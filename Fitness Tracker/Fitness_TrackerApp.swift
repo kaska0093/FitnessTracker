60cd1dfec3d7bd3500331ea7a325fcdbb57aa16d
Nikita <kaska0093@inbox.ru>
2025-02-19 02:57:41 -0800

    Initial Commit

---
+++ Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Nikita Shestakov on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
